2d92903c8bf162bfa68ee52419f3751f4d8f7cdb
Tygh Walters <tyghwalters@gmail.com>
2022-08-04 11:32:08 -0700

    Insert players as objects into the new game instance
    When creating a new Tic Tac Toe game, pass arguments (player1, player2) into the game's initialize method. Test = working! Add :player1 and :player2 to attr_reader

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,7 +1,4 @@
-# Goal: Build a tic-tac-toe game on the command line 
-# where two human players can play against each other w/
-# board displayed between turns.
-
+# Tic Tac Toe
 module TestMessage # module test - convert to something useful?
   TTT = "Tic Tac Toe" # constant to be used by instances/ both classes
   def working_message(class_string)
@@ -12,7 +9,7 @@ end
 class TictTacToe
   include TestMessage # module test - convert to something useful?
   # instance variables (as symbols) to attr_XYZ.
-  attr_reader :first, :second, :third, :fourth, :fifth, :sixth, :seventh, :eigth, :ninth
+  attr_reader :first, :second, :third, :fourth, :fifth, :sixth, :seventh, :eigth, :ninth, :player1, :player2
   attr_writer # any here?
   attr_accessor :board, :board_test, :winner
 
@@ -28,24 +25,27 @@ class TictTacToe
     [2,5,8], 
     [0,4,8], 
     [6,4,2]] 
-  @@available_picks = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  @@available_picks = [1, 2, 3, 4, 5, 6, 7, 8, 9] # not using this anywhere yet
   # if a user enters an unpickable number, return an error message
 
-  def initialize # constructor, because it is a special method
-    # that builds the object when a new object is instantiated.
-    # Gets triggered by the `new` class method.
+  def initialize (player1_obj, player2_obj)
     @@game_count += 1 # incrementing the class variable to count # games (for practice)
     puts "Starting a new #{TTT} game..." # TTT constant from the module
     @board = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] # array of which chars are printed to the game board 
     @board_test = ['1', '2', 'X', '4', 'X', '6', 'X', '8', '9']
     @winner = false
-  end
+    @player1 = player1_obj # inserting player object for access to attributes within the game
+    @player2 = player2_obj # inserting player object for access to attributes within the game
+    puts "Player 1, aka #{player1.name} object is loaded..." # test for obj. load is working!
+    puts "Player 2, aka #{player2.name} object is loaded..." # test for obj. load is working!
 
-  public # makes the methods below accesible from outside the class, until `private`
+  end
 
   def self.total_number_of_games # Class method to read the class variable
     @@game_count
   end
+
+  public # makes the methods below accesible from outside the class, until `private`
 
   def change_board(player_pick)
     board[player_pick] = "X" # replacing the board number with player's symbol 
@@ -107,41 +107,20 @@ class Player
   def turn()
     # after displaying the new board, get player to enter pick
     # replace the # of their pick with their shape
-    # pick =  1 # later, gets.chomp()
-
     puts "OK player #{"1"}, enter your pick # then hit return"
     player_pick = gets.chomp.to_i - 1 # local variable
-      # write code that errors if it's not available, maybe in the change_board method?
-    puts "player picked #{player_pick}"
-    player_pick # the pick needs to be the last thing so the fxn returns it's value for the next function
-    # Error here, not sure how to interact between classes...
-    # self.first = 'X' # using self here will call the instance method 
-    # and disambiguate from setting a local variable.
-    # puts "Here's the updated #{TTT} board..."
-    # newGame.show_board
+      # write code that errors if it's not available, in the change_board method?
+    puts "player picked #{player_pick + 1}"  # +1 to match board visual, not the array position
+    player_pick # needs to be last so the fxn returns it's value for the next fxn (change_board)
   end
 
 end# Class end
 
 
-# puts "New TICTACTOE game? Y/N"
-# reply = gets.chomp
-# while reply == "Y" 
-#   puts "great!"
-#   tictactoe = Game.new
-
-# else
-#   puts "Don't want to play?"
-#   sleep (1)
-#   puts "New TICTACTOE game? Y/N"
-#   reply = gets.chomp
-# end 
-
 # GAME START SEQUENCE
 puts "
 \
 Hello and welcome to Tygh's TIC TAC TOE game!"
-sleep 1
 puts "Player 1 will be X, please type your name then hit return."
 p1_name = gets.chomp
 sleep 0.5
@@ -158,7 +137,7 @@ sleep 1
 puts player1.shape # this is a test, it's working
 puts player2.shape # this is a test, it's working
 
-newGame = TictTacToe.new
+newGame = TictTacToe.new(player1, player2) # Passing player objects to have access to name and symbol for picks
 
 newGame.show_board
 
@@ -167,6 +146,7 @@ newGame.working_message("Player") # this is a test. Working!
 puts " This is game ##{TictTacToe.total_number_of_games}" # Calling our class method test. Working!
 
 newGame.change_board(player1.turn)
+newGame.change_board(player2.turn)
 # player2.turn
 # player1.turn
 # player2.turn
@@ -177,3 +157,18 @@ newGame.change_board(player1.turn)
 
 # until winner = true
   # Keep alternating players picking
+
+
+
+  # puts "New TICTACTOE game? Y/N"
+# reply = gets.chomp
+# while reply == "Y" 
+#   puts "great!"
+#   tictactoe = Game.new
+
+# else
+#   puts "Don't want to play?"
+#   sleep (1)
+#   puts "New TICTACTOE game? Y/N"
+#   reply = gets.chomp
+# end 
